dba0ca9e267eadd8173b40c74c1c7208929c784f
Rishank Jain <jainrishank20@gmail.com>
2026-07-02 17:15:23 +0530

    Cleanup: remove debug files, unused scripts, old screenshots

--- START.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Starting Client Tracker MOFSL...
-echo Dashboard will open in your browser automatically.
-echo Press Ctrl+C to stop.
-echo.
-streamlit run "%~dp0app.py" --server.port 8501 --browser.gatherUsageStats false
-pause
